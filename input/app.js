89b5db51cc19f0aa1b43ec08990e325f61f6dddd
abdillahhamzahali <abdillahhamzahali@gmail.com>
2023-03-10 19:47:24 +0700

    update domain projects

--- input/app.js
+++ input/app.js
@@ -25,28 +25,3 @@ const simpan = (title, description, created, teknologi, role, domain, img) => {
 };
 
 module.exports = { tulispertanyaan, simpan };
-// const input = () => {
-//   console.log("Jangan gunakan spasi pada saat memasukan Role!!");
-//   rl.question("Judul : ", (title) => {
-//     rl.question("dekripsi : ", (description) => {
-//       rl.question("tanggal dibuat : ", (created) => {
-//         rl.question("teknologi : ", (teknologi) => {
-//           rl.question("role : ", (role) => {
-//             rl.question("domain : ", (domain) => {
-//               rl.question("img : ", (img) => {
-//                 const dataInput = { title, description, created, teknologi, role, domain, img: `./imgs/Contoh Project/${img}` };
-//                 const file = fs.readFileSync("data/projects.json", "utf8");
-//                 const projects = JSON.parse(file);
-//                 projects.push(dataInput);
-//                 fs.writeFileSync("data/projects.json", JSON.stringify(projects));
-//                 console.log("terima kasih data sudah dimasukan");
-//                 rl.close();
-//               });
-//             });
-//           });
-//         });
-//       });
-//     });
-//   });
-// };
-// input();
